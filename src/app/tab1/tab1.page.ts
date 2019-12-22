7f42b97a41fb2cc69c18f9b165b67e0028121d5c
@mine <elwadiamine@gmail.com>
2019-12-22 09:41:13 +0100

    update

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { Globalization } from '@ionic-native/globalization/ngx';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  public title: string;
+  public title_2: string;
+  public description: string;
+  public name: string;
+  public language: string;
+  
+  constructor(private globalization: Globalization, private _translate: TranslateService) {
+
+  }
+
+  ionViewDidEnter(): void {
+    console.log('challa?')
+    this.getDeviceLanguage()
+  }
+
+  _initialiseTranslation(): void {
+    this._translate.get('TITLE').subscribe((res: string) => {
+      console.log(res);
+      this.title = res;
+    });
+    this._translate.get('description').subscribe((res: string) => {
+      console.log(res);
+      this.description = res;
+    });
+    this._translate.get('TITLE_2', { value: 'John' }).subscribe((res: string) => {
+      console.log(res);
+      this.title_2 = res;
+    });
+    this._translate.get('data.name', { name_value: 'Marissa Mayer' }).subscribe((res: string) => {
+      console.log(res);
+      this.name = res;
+    });
+
+  }
+
+  public changeLanguage(): void {
+    this._translateLanguage();
+  }
+
+  _translateLanguage(): void {
+    console.log('language', this.language)
+    this._translate.use(this.language);
+    this._initialiseTranslation();
+  }
+
+  _initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    this._translate.setDefaultLang('en');
+
+    if (this._translate.getBrowserLang() !== undefined) {
+      this.language = this._translate.getBrowserLang();
+      console.log('browser language is', this._translate.getBrowserLang());
+    }
+    else {
+      // Set your language here
+      this.language = 'en';
+    }
+
+    this._translateLanguage();
+  }
+
+  getDeviceLanguage() {
+    this.globalization.getPreferredLanguage()
+      .then(res => {
+        console.log(res)
+        this._initTranslate()
+      })
+      .catch(e => console.log(e));
+  }
+}
